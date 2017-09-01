6042f8c867f25ec8af42cb7a277d499aecc30de1
Marcel <mk@freeshell.de>
2017-09-01 15:24:03 +0200

    Update German translation

--- locale/dsblogoutmgr_de.ts
+++ locale/dsblogoutmgr_de.ts
@@ -4,58 +4,63 @@
 <context>
     <name>Countdown</name>
     <message>
-        <location filename="src/countdown.cpp" line="25"/>
+        <location filename="../src/countdown.cpp" line="55"/>
         <source>Cancel timer</source>
         <translation>Timer abbrechen</translation>
     </message>
     <message>
-        <location filename="src/countdown.cpp" line="26"/>
+        <location filename="../src/countdown.cpp" line="56"/>
         <source>Ok</source>
         <translation>Ok</translation>
     </message>
     <message>
-        <location filename="src/countdown.cpp" line="66"/>
-        <location filename="src/countdown.cpp" line="77"/>
-        <location filename="src/countdown.cpp" line="82"/>
+        <location filename="../src/countdown.cpp" line="57"/>
+        <source>Change timer</source>
+        <translation>Timer ändern</translation>
+    </message>
+    <message>
+        <location filename="../src/countdown.cpp" line="95"/>
+        <location filename="../src/countdown.cpp" line="106"/>
+        <location filename="../src/countdown.cpp" line="111"/>
         <source>Shutdown in %1 %2</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="src/countdown.cpp" line="68"/>
+        <location filename="../src/countdown.cpp" line="97"/>
         <source>Second</source>
         <translation>Sekunde</translation>
     </message>
     <message>
-        <location filename="src/countdown.cpp" line="68"/>
+        <location filename="../src/countdown.cpp" line="97"/>
         <source>Seconds</source>
         <translation>Sekunden</translation>
     </message>
     <message>
-        <location filename="src/countdown.cpp" line="71"/>
+        <location filename="../src/countdown.cpp" line="100"/>
         <source>Shutdown in %1 %2 %3 %4</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="src/countdown.cpp" line="73"/>
-        <location filename="src/countdown.cpp" line="79"/>
+        <location filename="../src/countdown.cpp" line="102"/>
+        <location filename="../src/countdown.cpp" line="108"/>
         <source>Hour</source>
         <translation>Stunde</translation>
     </message>
     <message>
-        <location filename="src/countdown.cpp" line="73"/>
-        <location filename="src/countdown.cpp" line="79"/>
+        <location filename="../src/countdown.cpp" line="102"/>
+        <location filename="../src/countdown.cpp" line="108"/>
         <source>Hours</source>
         <translation>Stunden</translation>
     </message>
     <message>
-        <location filename="src/countdown.cpp" line="75"/>
-        <location filename="src/countdown.cpp" line="84"/>
+        <location filename="../src/countdown.cpp" line="104"/>
+        <location filename="../src/countdown.cpp" line="113"/>
         <source>Minute</source>
         <translation>Minute</translation>
     </message>
     <message>
-        <location filename="src/countdown.cpp" line="75"/>
-        <location filename="src/countdown.cpp" line="84"/>
+        <location filename="../src/countdown.cpp" line="104"/>
+        <location filename="../src/countdown.cpp" line="113"/>
         <source>Minutes</source>
         <translation>Minuten</translation>
     </message>
@@ -63,32 +68,32 @@
 <context>
     <name>Mainwin</name>
     <message>
-        <location filename="src/mainwin.cpp" line="20"/>
+        <location filename="../src/mainwin.cpp" line="48"/>
         <source>Timer</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="src/mainwin.cpp" line="21"/>
+        <location filename="../src/mainwin.cpp" line="49"/>
         <source>Logout</source>
         <translation>Abmelden</translation>
     </message>
     <message>
-        <location filename="src/mainwin.cpp" line="23"/>
+        <location filename="../src/mainwin.cpp" line="51"/>
         <source>Reboot system</source>
         <translation>Neustart</translation>
     </message>
     <message>
-        <location filename="src/mainwin.cpp" line="25"/>
+        <location filename="../src/mainwin.cpp" line="53"/>
         <source>Shutdown system</source>
         <translation>Herunterfahren</translation>
     </message>
     <message>
-        <location filename="src/mainwin.cpp" line="27"/>
+        <location filename="../src/mainwin.cpp" line="55"/>
         <source>Suspend system</source>
         <translation>Ruhemodus</translation>
     </message>
     <message>
-        <location filename="src/mainwin.cpp" line="28"/>
+        <location filename="../src/mainwin.cpp" line="56"/>
         <source>Cancel</source>
         <translation>Abbrechen</translation>
     </message>
@@ -96,27 +101,27 @@
 <context>
     <name>Timerwin</name>
     <message>
-        <location filename="src/timerwin.cpp" line="13"/>
+        <location filename="../src/timerwin.cpp" line="40"/>
         <source>Set shutdown time in hours and/or minutes:</source>
         <translation>Setze Shutdown-Timer in Stunden und/oder Minuten:</translation>
     </message>
     <message>
-        <location filename="src/timerwin.cpp" line="20"/>
+        <location filename="../src/timerwin.cpp" line="47"/>
         <source>&amp;Ok</source>
         <translation>&amp;Ok</translation>
     </message>
     <message>
-        <location filename="src/timerwin.cpp" line="21"/>
+        <location filename="../src/timerwin.cpp" line="48"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Abbrechen</translation>
     </message>
     <message>
-        <location filename="src/timerwin.cpp" line="24"/>
+        <location filename="../src/timerwin.cpp" line="51"/>
         <source> Hours</source>
         <translation> Stunden</translation>
     </message>
     <message>
-        <location filename="src/timerwin.cpp" line="25"/>
+        <location filename="../src/timerwin.cpp" line="52"/>
         <source> Minutes</source>
         <translation> Minuten</translation>
     </message>
