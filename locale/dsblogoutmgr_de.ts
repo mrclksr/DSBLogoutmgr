8f7c147d02c4d316e4cf1ba839c3a31889bc8b64
Marcel <mk@freeshell.de>
2018-11-12 00:24:20 +0100

    Add german translations

--- locale/dsblogoutmgr_de.ts
+++ locale/dsblogoutmgr_de.ts
@@ -19,109 +19,175 @@
         <translation>Timer ändern</translation>
     </message>
     <message>
-        <location filename="../src/countdown.cpp" line="95"/>
-        <location filename="../src/countdown.cpp" line="106"/>
-        <location filename="../src/countdown.cpp" line="111"/>
+        <location filename="../src/countdown.cpp" line="97"/>
+        <location filename="../src/countdown.cpp" line="108"/>
+        <location filename="../src/countdown.cpp" line="113"/>
         <source>Shutdown in %1 %2</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/countdown.cpp" line="97"/>
+        <location filename="../src/countdown.cpp" line="99"/>
         <source>Second</source>
         <translation>Sekunde</translation>
     </message>
     <message>
-        <location filename="../src/countdown.cpp" line="97"/>
+        <location filename="../src/countdown.cpp" line="99"/>
         <source>Seconds</source>
         <translation>Sekunden</translation>
     </message>
     <message>
-        <location filename="../src/countdown.cpp" line="100"/>
+        <location filename="../src/countdown.cpp" line="102"/>
         <source>Shutdown in %1 %2 %3 %4</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/countdown.cpp" line="102"/>
-        <location filename="../src/countdown.cpp" line="108"/>
+        <location filename="../src/countdown.cpp" line="104"/>
+        <location filename="../src/countdown.cpp" line="110"/>
         <source>Hour</source>
         <translation>Stunde</translation>
     </message>
     <message>
-        <location filename="../src/countdown.cpp" line="102"/>
-        <location filename="../src/countdown.cpp" line="108"/>
+        <location filename="../src/countdown.cpp" line="104"/>
+        <location filename="../src/countdown.cpp" line="110"/>
         <source>Hours</source>
         <translation>Stunden</translation>
     </message>
     <message>
-        <location filename="../src/countdown.cpp" line="104"/>
-        <location filename="../src/countdown.cpp" line="113"/>
+        <location filename="../src/countdown.cpp" line="106"/>
+        <location filename="../src/countdown.cpp" line="115"/>
         <source>Minute</source>
         <translation>Minute</translation>
     </message>
     <message>
-        <location filename="../src/countdown.cpp" line="104"/>
-        <location filename="../src/countdown.cpp" line="113"/>
+        <location filename="../src/countdown.cpp" line="106"/>
+        <location filename="../src/countdown.cpp" line="115"/>
         <source>Minutes</source>
         <translation>Minuten</translation>
     </message>
 </context>
 <context>
+    <name>Delay</name>
+    <message>
+        <location filename="../src/delay.cpp" line="47"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <location filename="../src/delay.cpp" line="88"/>
+        <source>second</source>
+        <translation>Sekunde</translation>
+    </message>
+    <message>
+        <location filename="../src/delay.cpp" line="88"/>
+        <source>seconds</source>
+        <translation>Sekunden</translation>
+    </message>
+</context>
+<context>
     <name>Mainwin</name>
     <message>
-        <location filename="../src/mainwin.cpp" line="48"/>
+        <location filename="../src/mainwin.cpp" line="51"/>
         <source>Timer</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="49"/>
+        <location filename="../src/mainwin.cpp" line="52"/>
+        <location filename="../src/mainwin.cpp" line="105"/>
         <source>Logout</source>
         <translation>Abmelden</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="51"/>
+        <location filename="../src/mainwin.cpp" line="55"/>
+        <location filename="../src/mainwin.cpp" line="150"/>
+        <source>Lock screen</source>
+        <translation>Bildschirm sperren</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="57"/>
+        <location filename="../src/mainwin.cpp" line="114"/>
         <source>Reboot system</source>
         <translation>Neustart</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="53"/>
+        <location filename="../src/mainwin.cpp" line="59"/>
+        <location filename="../src/mainwin.cpp" line="124"/>
         <source>Shutdown system</source>
         <translation>Herunterfahren</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="55"/>
+        <location filename="../src/mainwin.cpp" line="61"/>
+        <location filename="../src/mainwin.cpp" line="134"/>
         <source>Suspend system</source>
         <translation>Ruhemodus</translation>
     </message>
     <message>
-        <location filename="../src/mainwin.cpp" line="56"/>
+        <location filename="../src/mainwin.cpp" line="105"/>
+        <source>Logging out in</source>
+        <translation>Abmelden in</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="114"/>
+        <source>Rebooting in</source>
+        <translation>Neustart in</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="124"/>
+        <source>Shutting down in</source>
+        <translation>Herunterfahren in</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="134"/>
+        <source>Suspending system in</source>
+        <translation>Ruhemodus in</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="150"/>
+        <source>Locking screen in</source>
+        <translation>Sperre Bildschirm in</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwin.cpp" line="53"/>
         <source>Cancel</source>
         <translation>Abbrechen</translation>
     </message>
 </context>
 <context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="167"/>
+        <source>Command &apos;%s&apos; not found</source>
+        <translation>Befehl &apos;%s&apos; nicht gefunden</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="171"/>
+        <source>Command &apos;%s&apos; exited with code %d</source>
+        <translation>Befehl &apos;%s&apos; mit Fehlercode %d beendet</translation>
+    </message>
+</context>
+<context>
     <name>Timerwin</name>
     <message>
-        <location filename="../src/timerwin.cpp" line="40"/>
+        <location filename="../src/timerwin.cpp" line="41"/>
         <source>Set shutdown time in hours and/or minutes:</source>
         <translation>Setze Shutdown-Timer in Stunden und/oder Minuten:</translation>
     </message>
     <message>
-        <location filename="../src/timerwin.cpp" line="47"/>
+        <location filename="../src/timerwin.cpp" line="48"/>
         <source>&amp;Ok</source>
         <translation>&amp;Ok</translation>
     </message>
     <message>
-        <location filename="../src/timerwin.cpp" line="48"/>
+        <location filename="../src/timerwin.cpp" line="49"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Abbrechen</translation>
     </message>
     <message>
-        <location filename="../src/timerwin.cpp" line="51"/>
+        <location filename="../src/timerwin.cpp" line="52"/>
         <source> Hours</source>
         <translation> Stunden</translation>
     </message>
     <message>
-        <location filename="../src/timerwin.cpp" line="52"/>
+        <location filename="../src/timerwin.cpp" line="53"/>
         <source> Minutes</source>
         <translation> Minuten</translation>
     </message>
